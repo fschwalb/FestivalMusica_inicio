f997e9f754ccad0c993b1658c5d7c64db52efd35
Fernando Schwalb <83603017+fschwalb@users.noreply.github.com>
2022-05-07 00:18:21 -0300

    app.js Added

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    iniciarApp();
+});
+
+function iniciarApp() {
+    
+    crearGaleria();
+};
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    galeria.textContent = 'Vamos a crear la galería';
+};
